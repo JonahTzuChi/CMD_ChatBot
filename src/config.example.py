fd44236cd8525f1d31b346744055ccb823b2aef8
Jonah TzCh <jonah_mytzuchi@outlook.com>
2023-11-26 23:50:57 +0800

    Add models

--- src/config.example.py
+++ src/config.example.py
@@ -3,4 +3,6 @@ OPENAI_API_KEY = "<API-KEY>"
 MODELS = [
     {"name": "gpt-3.5-turbo", "in": "0.003/1k", "out": "0.006/1k"},
     {"name": "gpt-4", "in": "", "out": "0.03/1k", "out": "0.06/1k"},
+    {"name": "gpt-4-1106-preview", "in": "0.01/1k", "out": "0.03/1k"},
+    {"name": "gpt-3.5-turbo-1106", "in": "0.001/1k", "out": "0.002/1k"}
 ]
