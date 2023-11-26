1a3afe13d4c9711dd89c0c446a9289f30c43f554
Jonah TzCh <jonah_mytzuchi@outlook.com>
2023-11-26 19:11:37 +0800

    offer a short list of models

--- src/config.example.py
+++ src/config.example.py
@@ -1,3 +1,6 @@
 TOKEN_THRESHOLD = 1000
 OPENAI_API_KEY = "<API-KEY>"
-MODEL = "gpt-3.5-turbo"
+MODELS = [
+    {"name": "gpt-3.5-turbo", "in": "0.003/1k", "out": "0.006/1k"},
+    {"name": "gpt-4", "in": "", "out": "0.03/1k", "out": "0.06/1k"},
+]
