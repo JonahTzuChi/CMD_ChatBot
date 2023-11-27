875c9114065109988bde6984e2d3278f94e554b0
Jonah TzCh <jonah_mytzuchi@outlook.com>
2023-11-27 23:23:00 +0800

    move read file related functions to another py file to improve readability and code structure

---
+++ src/lib/freader.py
@@ -0,0 +1,88 @@
+import os
+import numpy as np
+import pandas as pd
+import PyPDF2
+import json
+
+
+def read_starndard_text_file(filename: str) -> str:
+    data = f"FileName:{filename}"
+    with open(filename, "r") as reader:
+        lines = reader.readlines()
+        data += "".join(lines)
+
+    return data
+
+
+def read_spreadsheet(filename: str) -> str:
+    data = f"FileName:{filename}"
+    workbook = pd.read_excel(filename, sheet_name=None)
+    for sheetname in workbook.keys():
+        df = workbook[sheetname]
+        data += f"\nSheetName:{sheetname}" + df.to_json()
+    return data
+
+
+def read_pdf(filename: str) -> str:
+    data = f"FileName:{filename}"
+    with open(filename, "rb") as fileObj:
+        pdfReader = PyPDF2.PdfReader(fileObj)
+        pages = pdfReader.pages
+        print(len(pages))
+        for page in pages:
+            txt = page.extract_text()
+            data += f"\nnewPage:" + txt
+    return data
+
+
+def read_npy_file(filename: str) -> str:
+    data = f"FileName:{filename}"
+    data = np.load(filename, encoding="ASCII")
+    data_list = data.tolist()
+    json_data = json.dumps(data_list)
+    return data + f"\n{json_data}"
+
+
+def pickTextFileReader(extension: str):
+    if extension in [
+        "txt",
+        "csv",
+        "py",
+        "js",
+        "json",
+        "sql",
+        "yml",
+        "env",
+        "h",
+        "cpp",
+        "cc",
+        "java",
+        "cs",
+        "html",
+        "php",
+        "rs",
+        "go",
+        "Dockerfile",
+    ]:
+        return read_starndard_text_file
+    if extension in ["xls", "xlsx"]:
+        return read_spreadsheet
+    if extension in ["pdf"]:
+        return read_pdf
+    if extension in ["npy"]:
+        return read_npy_file
+    raise "Invalid File Extension"
+
+
+def readFile(filename: str) -> str:
+    try:
+        if ~os.path.exists(filename):
+            return "FileIOError"
+
+        extension = filename.split(".")[-1]
+        fileReader = pickTextFileReader(extension)
+
+        return fileReader(filename)
+    except Exception as e:
+        print(str(e), flush=True)
+        raise e
--- src/lib/utils.py
+++ src/lib/utils.py
@@ -1,15 +1,10 @@
-import os
 import re
 from enum import Enum
 import requests
-import pandas as pd
-import PyPDF2
-import numpy as np
-import json
 
 import config
 from lib.custom_exception import OpenAIRequestError, OpenAIResponseParsingError
-
+from freader import readFile
 
 class Roles(Enum):
     SYSTEM = "system"
@@ -119,89 +114,6 @@ def export_chat_history(
     return EXPORT_PATH
 
 
-def read_starndard_text_file(filename: str) -> str:
-    data = f"FileName:{filename}"
-    with open(filename, "r") as reader:
-        lines = reader.readlines()
-        data += "".join(lines)
-
-    return data
-
-
-def read_spreadsheet(filename: str) -> str:
-    data = f"FileName:{filename}"
-    workbook = pd.read_excel(filename, sheet_name=None)
-    for sheetname in workbook.keys():
-        df = workbook[sheetname]
-        data += f"\nSheetName:{sheetname}" + df.to_json()
-    return data
-
-
-def read_pdf(filename: str) -> str:
-    data = f"FileName:{filename}"
-    with open(filename, "rb") as fileObj:
-        pdfReader = PyPDF2.PdfReader(fileObj)
-        pages = pdfReader.pages
-        print(len(pages))
-        for page in pages:
-            txt = page.extract_text()
-            data += f"\nnewPage:" + txt
-    return data
-
-
-def read_npy_file(filename: str) -> str:
-    data = f"FileName:{filename}"
-    data = np.load(filename, encoding="ASCII")
-    data_list = data.tolist()
-    json_data = json.dumps(data_list)
-    return data + f"\n{json_data}"
-
-
-def pickTextFileReader(extension: str):
-    if extension in [
-        "txt",
-        "csv",
-        "py",
-        "js",
-        "json",
-        "sql",
-        "yml",
-        "env",
-        "h",
-        "cpp",
-        "cc",
-        "java",
-        "cs",
-        "html",
-        "php",
-        "rs",
-        "go",
-        "Dockerfile",
-    ]:
-        return read_starndard_text_file
-    if extension in ["xls", "xlsx"]:
-        return read_spreadsheet
-    if extension in ["pdf"]:
-        return read_pdf
-    if extension in ["npy"]:
-        return read_npy_file
-    raise "Invalid File Extension"
-
-
-def readFile(filename: str) -> str:
-    try:
-        if ~os.path.exists(filename):
-            return "FileIOError"
-
-        extension = filename.split(".")[-1]
-        fileReader = pickTextFileReader(extension)
-
-        return fileReader(filename)
-    except Exception as e:
-        print(str(e), flush=True)
-        raise e
-
-
 def start():
     model = pick_model()
     print(f">Model = {model}")
@@ -244,8 +156,8 @@ def start():
             if re.search("^--file ", user_feedback) is not None:
                 filename = user_feedback.replace("--file ", "")
                 user_feedback = readFile(filename)
-                while user_feedback == "FIleIOError":
-                    print(f"File cannot be found at {filename}")
+                while user_feedback in ["FIleIOError", "Invalid File Extension"]:
+                    print(f"{user_feedback}")
                     user_feedback = input()
                     user_feedback = user_feedback.strip()
                     if user_feedback == "q":
